ecc9c6ce680ba46f352f3831c44bacc1733f7918
André Luís <andre.luis.2018@outlook.com>
2022-10-14 18:31:19 -0300

    parte de adicionar produtos/listar produtos iniciada

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -11,25 +11,19 @@ class SessionController {
       if (usuario === usuarios[i].login && senha === usuarios[i].senha) {
         // eslint-disable-next-line no-unused-vars
         const { cargo } = usuarios[i];
-<<<<<<< HEAD
+
         res.status(200).send(usuario);
         return;
-<<<<<<< HEAD
       }
       c += 1;
       if (c === usuarios.length) {
         res.status(404).send(false);
-        return;
-=======
-=======
         res.json({ ok: true });
->>>>>>> fa82dda1c1a50e725c76f2e7d7f259bce6db3424
-      } else {
-        c += 1;
-        if (c === usuarios.length) {
-          res.json({ not: false });
-        }
->>>>>>> f7479d511ecaaba3fa15e5a9e95fb50e113728d0
+        return;
+      }
+      c += 1;
+      if (c === usuarios.length) {
+        res.json({ not: false });
       }
     }
   }
