8b73ad55abdfbb9966b42d3edff36621a404bae6
André Luís <96019831+Andreprogdev@users.noreply.github.com>
2022-10-23 20:53:16 -0300

    Update create_user.js

--- controllers/create_user.js
+++ controllers/create_user.js
@@ -2,10 +2,13 @@
 const fs = require('fs');
 const yup = require('yup');
 const usuarios = require('../usuarios.json');
-const jwt = require('jsonwebtoken');
 
 const data = fs.readFileSync('./usuarios.json', 'utf-8');
 const user = JSON.parse(data);
+
+function createId() {
+  return Math.random();
+}
 
 class createUser {
   async update(req, res) {
@@ -14,21 +17,33 @@ class createUser {
       senha: yup.number().required(),
       cargo: yup.string().required(),
       email: yup.string().email().required(),
+      regiao: yup.string(),
     });
     const { login } = req.body;
     const { senha } = req.body;
     const { cargo } = req.body;
     const { email } = req.body;
+    const { regiao } = req.body;
+    const id = createId();
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Falha na validação do cadastro' });
     }
 
+    for (let i = 0; i < usuarios.length; i += 1) {
+      if (login === usuarios[i].login) {
+        return res.json({ error: 'usuário ja cadastrado' });
+      }
+      if (email === usuarios[i].email) {
+        return res.json({ error: 'email já cadastrado' });
+      }
+    }
+
     await user.push({
-      login, senha, cargo, email,
+      login, senha, cargo, email, id, regiao,
     });
     fs.writeFileSync('./usuarios.json', JSON.stringify(user, null), 'utf-8');
-    return res.json({ token: jwt.sing({ login }, '0ee4647cf7ecd7c5167616140b538ccf',{expiresIn: '3d',}), });
+    return res.json({ ok: true });
   }
 }
 
